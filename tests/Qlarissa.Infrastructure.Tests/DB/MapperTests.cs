a5309a70ed90777eb324fe49101cdc7a55fafe47
sebastian.br <sebastian.br@web.de>
2025-09-24 01:52:57 +0200

    new file for mapper tests

---
+++ tests/Qlarissa.Infrastructure.Tests/DB/MapperTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Qlarissa.Infrastructure.Tests.DB;
+
+public class MapperTests
+{
+    [Fact]
+    public void MapCurrency_FromDomainEntity()
+    {
+
+    }
+
+    [Fact]
+    public void MapCurrency_ToDomainEntity()
+    {
+
+    }
+}
