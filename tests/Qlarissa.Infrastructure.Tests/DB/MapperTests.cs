cfe2e4cf0135b162e828a4e3ec36181b6c1cee97
sebastian.br <sebastian.br@web.de>
2025-09-26 01:27:19 +0200

    added Currency mapping tests

--- tests/Qlarissa.Infrastructure.Tests/DB/MapperTests.cs
+++ tests/Qlarissa.Infrastructure.Tests/DB/MapperTests.cs
@@ -1,16 +1,27 @@
-﻿namespace Qlarissa.Infrastructure.Tests.DB;
+﻿using Qlarissa.Domain.Entities.Securities;
+using Qlarissa.Infrastructure.DB.Entities;
+
+namespace Qlarissa.Infrastructure.Tests.DB;
 
 public class MapperTests
 {
     [Fact]
     public void MapCurrency_FromDomainEntity()
     {
-
+        Domain.Entities.Securities.Currency domainEntity = new() { Id = 1, Symbol = "USD", Name = "US Dollar" };
+        var dbEntity = Infrastructure.DB.Entities.Currency.FromDomainEntity(domainEntity);
+        Assert.Equal(domainEntity.Id, dbEntity.Id);
+        Assert.Equal(domainEntity.Symbol, dbEntity.Symbol);
+        Assert.Equal(domainEntity.Name, dbEntity.Name);
     }
 
     [Fact]
     public void MapCurrency_ToDomainEntity()
     {
-
+        Infrastructure.DB.Entities.Currency dbEntity = new() { Id = 1, Symbol = "USD", Name = "US Dollar" };
+        var domainEntity = dbEntity.ToDomainEntity();
+        Assert.Equal(dbEntity.Id, domainEntity.Id);
+        Assert.Equal(dbEntity.Symbol, domainEntity.Symbol);
+        Assert.Equal(dbEntity.Name, domainEntity.Name);
     }
 }
