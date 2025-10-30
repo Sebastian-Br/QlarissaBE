9a9ccfdae2436720ca85e80876a2721c3bb0fbaf
sebastian.br <sebastian.br@web.de>
2025-10-30 02:28:04 +0100

    added DateOnly<->Double conversion

---
+++ src/Qlarissa.Application/Extensions/DateOnlyExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Qlarissa.Application.Extensions;
+
+public static class DateOnlyExtensions
+{
+    public static double ToDouble(this DateOnly date)
+    {
+        int year = date.Year;
+        int daysInYear = DateTime.IsLeapYear(year) ? 366 : 365;
+        return year + date.DayOfYear / (double) daysInYear;
+    }
+
+    public static DateOnly ToDateOnly(this double date)
+    {
+        int year = (int)Math.Floor(date);
+        double fractionOfYear = date - year;
+        int daysInYear = DateTime.IsLeapYear(year) ? 366 : 365;
+        int dayOfYear = (int)Math.Round(fractionOfYear * daysInYear);
+        return new DateOnly(year, 1, 1).AddDays(dayOfYear - 1);
+    }
+}
---
+++ tests/Qlarissa.Application.Tests/DateOnlyExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using Qlarissa.Application.Extensions;
+
+namespace Qlarissa.Application.Tests;
+
+public class DateOnlyExtensionsTests
+{
+    [Fact]
+    public void DateOnly_ToDouble_Equals_ToDateOnly()
+    {
+        DateOnly currentDate = new(2020, 1, 1); // includes a leap year and a non leap year
+        while (currentDate.Year < 2022)
+        {
+            Assert.Equal(currentDate, currentDate.ToDouble().ToDateOnly());
+            currentDate = currentDate.AddDays(1);
+        }
+    }
+}
