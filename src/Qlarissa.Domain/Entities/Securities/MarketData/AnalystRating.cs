3799ef0ac5f4d63624ccd3d27f2faf2fdcd6d323
Sebastian Br <Sebastian @ Br>
2026-08-20 21:31:33 +0200

    removed RatingKey

--- src/Qlarissa.Domain/Entities/Securities/MarketData/AnalystRating.cs
+++ src/Qlarissa.Domain/Entities/Securities/MarketData/AnalystRating.cs
@@ -2,18 +2,9 @@
 
 public sealed class AnalystRating
 {
-    public RatingKey RatingKey { get; set; }
-
     public int NumberOfAnalystOpinions { get; set; }
 
     public double TargetMeanPrice { get; set; }
 
     public double RecommendationMean {  get; set; }
-}
-
-public enum RatingKey
-{
-    Buy = 1,
-    Hold = 2,
-    Sell = 3
 }
