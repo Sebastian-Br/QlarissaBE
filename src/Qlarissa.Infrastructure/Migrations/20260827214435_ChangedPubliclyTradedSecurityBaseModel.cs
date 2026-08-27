b3ce86a510d0d071a29e79bfb7d30a6760427373
Sebastian Br <Sebastian @ Br>
2026-08-27 23:45:42 +0200

    added migration

---
+++ src/Qlarissa.Infrastructure/Migrations/20260827214435_ChangedPubliclyTradedSecurityBaseModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Qlarissa.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedPubliclyTradedSecurityBaseModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExchangeName",
+                table: "SecurityBase",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExchangeShortName",
+                table: "SecurityBase",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SecurityType",
+                table: "SecurityBase",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExchangeName",
+                table: "SecurityBase");
+
+            migrationBuilder.DropColumn(
+                name: "ExchangeShortName",
+                table: "SecurityBase");
+
+            migrationBuilder.DropColumn(
+                name: "SecurityType",
+                table: "SecurityBase");
+        }
+    }
+}
